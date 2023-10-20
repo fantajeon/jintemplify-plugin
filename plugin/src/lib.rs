1cc891363ff8649b5c7a224447b48bea3a8b17c8
Hyeokjune Jeon <hyeokjune.jeon@furiosa.ai>
2023-10-20 13:18:17 +0900

    plugin library

---
+++ plugin/src/lib.rs
@@ -0,0 +1,84 @@
+use serde::Serialize;
+
+#[macro_export]
+macro_rules! guest_plugin {
+    () => {
+        #[no_mangle]
+        extern "C" {
+            fn print_log_from_wasm(ptr: *const u8, len: usize);
+        }
+
+        #[no_mangle]
+        extern "C" fn guest_free(ptr: *mut plugin::ReturnValues) {
+            free_return_values(ptr);
+        }
+
+        pub fn free_return_values(ptr: *mut plugin::ReturnValues) {
+            let boxed = unsafe { Box::from_raw(ptr) };
+            let _ = unsafe {
+                Vec::from_raw_parts(boxed.ptr as *mut u8, boxed.len as usize, boxed.cap as usize)
+            };
+        }
+
+        pub fn send_log(message: &str) {
+            unsafe {
+                print_log_from_wasm(message.as_ptr(), message.len());
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! host_plugin {
+    () => {};
+}
+
+#[allow(dead_code)]
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct InputWrapper {
+    pub params: Vec<serde_json::Value>,
+}
+
+#[allow(dead_code)]
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct OutputWrapper {
+    pub result: serde_json::Value,
+}
+
+#[allow(dead_code)]
+#[repr(C)]
+pub struct ReturnValues {
+    pub ptr: u32,
+    pub len: u32,
+    pub cap: u32,
+}
+
+#[allow(dead_code)]
+#[derive(Serialize)]
+pub struct ErrorValue {
+    pub reason: String,
+}
+
+pub fn serialize_to_return_values<T: serde::Serialize>(data: &T) -> *mut ReturnValues {
+    let output_json = serde_json::to_string(data).expect("Failed to serialize data to JSON");
+    let output_bytes = output_json.into_bytes();
+    let output_len = output_bytes.len();
+    let output_ptr = output_bytes.as_ptr();
+    let output_cap = output_bytes.capacity();
+    std::mem::forget(output_bytes);
+
+    let return_values = Box::new(ReturnValues {
+        ptr: output_ptr as u32,
+        len: output_len as u32,
+        cap: output_cap as u32,
+    });
+    Box::into_raw(return_values)
+}
+
+pub fn convert_value<T: serde::de::DeserializeOwned>(
+    value: &serde_json::Value,
+    index: usize,
+) -> Result<T, String> {
+    serde_json::from_value(value.clone())
+        .map_err(|err| format!("Error at index {}: {}", index, err))
+}
---
+++ plugin_macro/src/lib.rs
@@ -0,0 +1,97 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, parse_quote, FnArg, ItemFn, Pat, ReturnType, Stmt};
+
+fn translate_inputs<'a>(it: impl Iterator<Item = &'a mut FnArg>) -> Vec<Stmt> {
+    let preprocess_block: Stmt = parse_quote! {
+        let args: plugin::InputWrapper = {
+            let slice = unsafe { std::slice::from_raw_parts(ptr, len as usize) };
+            let json_str = match std::str::from_utf8(slice) {
+                Ok(s) => s,
+                Err(_) => {
+                    return plugin::serialize_to_return_values(&plugin::ErrorValue {
+                        reason: "Failed to convert byte slice to string".to_string(),
+                    })
+                }
+            };
+            let args = match serde_json::from_str(json_str) {
+                Ok(val) => val,
+                Err(err) => {
+                    return plugin::serialize_to_return_values(&plugin::ErrorValue {
+                        reason: format!("Failed to deserialize JSON: {}", err).to_string(),
+                    })
+                }
+            };
+            args
+        };
+    };
+
+    let mut out: Vec<Stmt> = vec![preprocess_block];
+
+    it.enumerate()
+        .map(|(i, arg)| {
+            let FnArg::Typed(arg) = arg else {
+                panic!("self is not allowed for plugin functions")
+            };
+            let Pat::Ident(id) = &*arg.pat else {
+                panic!("Invalid function declation")
+            };
+            (i, id.ident.clone(), &mut arg.ty)
+        })
+        .for_each(|(index, name, ty)| {
+            out.push(
+                parse_quote!(let #name: #ty = plugin::convert_value::<#ty>(&args.params[#index], #index).unwrap();),
+            );
+        });
+
+    out
+}
+
+fn translate_output(ret: &mut ReturnType) -> Stmt {
+    let mut out = parse_quote!(return (out.ptr, out.len););
+
+    if let ReturnType::Type(_, _ty) = ret {
+        out = parse_quote!({
+            let out = plugin::OutputWrapper {
+                result: serde_json::json!(out),
+            };
+            return plugin::serialize_to_return_values(&out);
+        });
+    }
+
+    out
+}
+
+fn make_plugin(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut item_fn = parse_macro_input!(item as ItemFn);
+
+    let prelude = translate_inputs(item_fn.sig.inputs.iter_mut());
+    let epilode = translate_output(&mut item_fn.sig.output);
+
+    let fn_name = &item_fn.sig.ident;
+    let fn_block = &item_fn.block;
+    let output_type = item_fn.sig.output.clone();
+
+    let expanded = quote! {
+        #[no_mangle]
+        pub unsafe extern "C" fn #fn_name (ptr: *mut u8, len: u32) -> *mut plugin::ReturnValues {
+            #(#prelude)*
+            let out = (move || #output_type #fn_block)();
+            #epilode
+        }
+    };
+
+    expanded.into()
+}
+
+#[proc_macro_attribute]
+pub fn plugin_filter(attr: TokenStream, item: TokenStream) -> TokenStream {
+    make_plugin(attr, item)
+}
+
+#[proc_macro_attribute]
+pub fn plugin_function(attr: TokenStream, item: TokenStream) -> TokenStream {
+    make_plugin(attr, item)
+}
